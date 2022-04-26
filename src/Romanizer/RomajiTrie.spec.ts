b908ed42c0ee086df502391864dfcd67555ca893
Shuhei YOSHIDA <contact@yantene.net>
2022-04-27 01:38:13 +0900

    Create RomajiTrie class

---
+++ src/Romanizer/RomajiTrie.spec.ts
@@ -0,0 +1,177 @@
+import { RomajiTrie } from "./RomajiTrie";
+
+describe(RomajiTrie, () => {
+  describe("size", () => {
+    test("空配列を引数として渡したとき、size は 0 であること", () => {
+      const romajiMap = new RomajiTrie([]);
+
+      expect(romajiMap.size).toBe(0);
+    });
+
+    test("重複しないカナローマ字ペアを 5 個コンストラクタに渡すと size は 5 であること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ア", "a"],
+        ["カ", "ka"],
+        ["サ", "sa"],
+        ["タ", "ta"],
+        ["ナ", "na"],
+      ]);
+
+      expect(romajiMap.size).toBe(5);
+    });
+
+    test("カナローマ字ペアを 5 個コンストラクタに渡し、そのうち 2 ペアのカナが重複していると size は 3 であること", () => {
+      const romajiMap = new RomajiTrie([
+        ["シ", "si"],
+        ["カ", "ka"],
+        ["シ", "shi"],
+        ["ヲ", "wo"],
+        ["ヲ", "o"],
+      ]);
+
+      expect(romajiMap.size).toBe(3);
+    });
+
+    test("カナローマ字ペアを 5 個コンストラクタに渡し、そのうち 2 ペアのローマ字が重複していても size は 5 であること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ンオ", "no"],
+        ["カ", "ka"],
+        ["ノ", "no"],
+        ["テ", "te"],
+        ["テー", "te"],
+      ]);
+
+      expect(romajiMap.size).toBe(5);
+    });
+  });
+
+  describe("#getGreedy()", () => {
+    test("コンストラクタに渡したカナローマ字ペアに含まれるカナを渡すと、全体にマッチし、対応するローマ字が返ること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ア", "a"],
+        ["カ", "ka"],
+        ["サ", "sa"],
+        ["タ", "ta"],
+        ["ナ", "na"],
+      ]);
+
+      [
+        { kana: "ア", romaji: "a" },
+        { kana: "カ", romaji: "ka" },
+        { kana: "サ", romaji: "sa" },
+        { kana: "タ", romaji: "ta" },
+        { kana: "ナ", romaji: "na" },
+      ].forEach(({ kana, romaji }) => {
+        const { romaji: answeredRomaji, matchedKana } =
+          romajiMap.getGreedy(kana);
+        expect(answeredRomaji).toBe(romaji);
+        expect(matchedKana).toBe(kana);
+      });
+    });
+
+    test("コンストラクタに渡したカナローマ字ペアに含まれないカナを渡すと、全体にマッチし、undefined が返ること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ハ", "ha"],
+        ["マ", "ma"],
+        ["ヤ", "ya"],
+        ["ラ", "ra"],
+        ["ワ", "wa"],
+      ]);
+
+      [
+        { kana: "ア", romaji: "a" },
+        { kana: "カ", romaji: "ka" },
+        { kana: "サ", romaji: "sa" },
+        { kana: "タ", romaji: "ta" },
+        { kana: "ナ", romaji: "na" },
+      ].forEach(({ kana, romaji }) => {
+        const { romaji: answeredRomaji, matchedKana } =
+          romajiMap.getGreedy(kana);
+        expect(answeredRomaji).toBeUndefined();
+        expect(matchedKana).toBeUndefined();
+      });
+    });
+
+    test("カナが重複するカナローマ字ペアをコンストラクタに渡して重複するカナを渡すと、全体にマッチし、より後ろのローマ字が返ること", () => {
+      const romajiMap = new RomajiTrie([
+        ["シ", "si"],
+        ["カ", "ka"],
+        ["シ", "shi"],
+        ["ヲ", "wo"],
+        ["ヲ", "o"],
+      ]);
+
+      [
+        { kana: "シ", romaji: "shi" },
+        { kana: "ヲ", romaji: "o" },
+      ].forEach(({ kana, romaji }) => {
+        const { romaji: answeredRomaji, matchedKana } =
+          romajiMap.getGreedy(kana);
+        expect(answeredRomaji).toBe(romaji);
+        expect(matchedKana).toBe(kana);
+      });
+    });
+
+    test("ローマ字が重複するカナローマ字ペアをコンストラクタに渡してカナを渡すと、全体にマッチし、対応するローマ字が返ること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ンオ", "no"],
+        ["カ", "ka"],
+        ["ノ", "no"],
+        ["テ", "te"],
+        ["テー", "te"],
+      ]);
+
+      [
+        { kana: "ンオ", romaji: "no" },
+        { kana: "カ", romaji: "ka" },
+        { kana: "ノ", romaji: "no" },
+        { kana: "テ", romaji: "te" },
+        { kana: "テー", romaji: "te" },
+      ].forEach(({ kana, romaji }) => {
+        const { romaji: answeredRomaji, matchedKana } =
+          romajiMap.getGreedy(kana);
+        expect(answeredRomaji).toBe(romaji);
+        expect(matchedKana).toBe(kana);
+      });
+    });
+
+    test("前方から部分一致をするカナのパターンが複数ある場合、より長くマッチすること", () => {
+      const romajiMap = new RomajiTrie([
+        ["ン", "n"],
+        ["ブ", "bu"],
+        ["ンブ", "mbu"],
+      ]);
+
+      const { romaji, matchedKana } = romajiMap.getGreedy("ンブン");
+
+      expect(romaji).toBe("mbu");
+      expect(matchedKana).toBe("ンブ");
+    });
+  });
+
+  describe("#chunk()", () => {
+    test("カナローマ字ペアの単位でチャンクが分割されること", () => {
+      const romajiMap = new RomajiTrie([
+        ["シ", "shi"],
+        ["ンブ", "mbu"],
+        ["ン", "n"],
+      ]);
+
+      const chunks = romajiMap.chunk("シンブンシ");
+
+      expect(chunks.map(({ romaji }) => romaji)).toEqual([
+        "shi",
+        "mbu",
+        "n",
+        "shi",
+      ]);
+
+      expect(chunks.map(({ matchedKana }) => matchedKana)).toEqual([
+        "シ",
+        "ンブ",
+        "ン",
+        "シ",
+      ]);
+    });
+  });
+});
---
+++ src/Romanizer/RomajiTrie.ts
@@ -0,0 +1,81 @@
+type Trie = Map<string, string | Trie>;
+
+export class RomajiTrie {
+  #trie: Trie = new Map<string, Trie | string>();
+  size: number = 0;
+
+  constructor(kanaRomajiArray: [string, string][]) {
+    kanaRomajiArray.forEach(([kana, romaji]) => {
+      this.#setToTrie(kana, romaji, this.#trie);
+    });
+  }
+
+  #setToTrie(kana: string, romaji: string, trie: Trie) {
+    if (kana === "") {
+      if (!trie.has("$")) this.size++;
+
+      trie.set("$", romaji);
+      return;
+    }
+
+    const subTrie =
+      (trie.get(kana.charAt(0)) as Trie | undefined) ??
+      new Map<string, string | Trie>();
+
+    trie.set(kana.charAt(0), subTrie);
+    this.#setToTrie(kana.substring(1), romaji, subTrie);
+  }
+
+  #getFromTrieGreedy(
+    text: string,
+    cursor: number,
+    trie: Trie | undefined
+  ): { romaji: string; matchedKana: string } | undefined {
+    // When no Trie left or no text left (including EOS)
+    if (trie === undefined || text.length < cursor) return undefined;
+
+    const subTrie = trie.get(text.charAt(cursor)) as Trie | undefined;
+    const result = this.#getFromTrieGreedy(text, cursor + 1, subTrie);
+
+    // When I have already find a longer match
+    if (result) return result;
+
+    const romaji = trie.get("$") as string | undefined;
+
+    // When this cursor didn't match
+    if (romaji === undefined) return undefined;
+
+    return { romaji, matchedKana: text.substring(0, cursor) };
+  }
+
+  getGreedy(
+    text: string
+  ):
+    | { romaji: string; matchedKana: string }
+    | { romaji: undefined; matchedKana: undefined } {
+    return (
+      this.#getFromTrieGreedy(text, 0, this.#trie) ?? {
+        romaji: undefined,
+        matchedKana: undefined,
+      }
+    );
+  }
+
+  chunk(text: string): { romaji?: string; matchedKana: string }[] {
+    const chunks = [];
+    let cursor = 0;
+    while (cursor < text.length) {
+      const remainingText = text.substring(cursor);
+      const { romaji, matchedKana } = this.getGreedy(remainingText);
+      if (romaji === undefined) {
+        chunks.push({ romaji, matchedKana: remainingText });
+        break;
+      }
+
+      chunks.push({ romaji, matchedKana });
+      cursor += matchedKana.length;
+    }
+
+    return chunks;
+  }
+}
